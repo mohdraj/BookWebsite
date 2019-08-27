ddc7f40e155b60b3c210590d64eca1ffc31203e8
Mohammad Uzair <54577733+mohdraj@users.noreply.github.com>
2019-08-27 16:31:59 +0530

    First Upload

---
+++ regi.js
@@ -0,0 +1,24 @@
+let Mail = prompt("Login using Email", '');
+mail=document.getElementById("mail").value
+pas=document.getElementById("pass").value
+if (Mail == mail) {
+
+  let pass = prompt('Password Plz', '');
+
+  if (pass == pas) {
+    alert( 'Welcome!' );
+  } else if (pass == '' || pass == null) {
+    alert( 'Cancelled' );
+    window.location.replace("regi.html");
+  } else {
+    alert( 'Wrong password' );
+    window.location.replace("regi.html");
+  }
+
+} else if (Mail == '' || Mail == null) {
+  alert( 'Cancelled' );
+  window.location.replace("regi.html");
+} else {
+  alert( "Email Not Registered" );
+  window.location.replace("regi.html");
+}
